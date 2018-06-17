aceed6ba5a2fdbf981cc1bd2dd72c4aded83c0b5
Mate Weidlich <weidlichmate@gmail.com>
2018-06-17 16:49:54 +0200

    feature: billing and credit handling

--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
-//modules
+// modules
 const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 
-//models
+// models
 require('./models/User');
 mongoose.connect(keys.mongoURI);
 
@@ -15,17 +16,31 @@ require('./services/passport');
 // app
 const app = express();
 
+app.use(bodyParser.json());
 app.use(
-	cookieSession({
-		maxAge: 30 * 24 * 60 * 60 * 1000,
-		keys: [keys.cookieKey]
-	})
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
 );
 app.use(passport.initialize());
 app.use(passport.session());
 
-//routes
+// routes
 require('./routes/auth')(app);
+require('./routes/billing')(app);
+
+// react
+if (process.env.NODE_ENV === 'production') {
+    // static
+    app.use(express.static('client/build'));
+
+    // react index.html
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 // server
 const PORT = process.env.PORT || 5000;
